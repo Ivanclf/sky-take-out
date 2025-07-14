e9be7e8abaf244975b46c328b4d527b792c850e7
Ivanclf <23009201447@stu.xidian.edu.cn>
2025-07-15 00:15:51 +0800

    公共字段代码填充功能实现

--- sky-server/src/main/java/com/sky/service/impl/CategoryServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/CategoryServiceImpl.java
@@ -4,7 +4,6 @@ import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
 import com.sky.constant.MessageConstant;
 import com.sky.constant.StatusConstant;
-import com.sky.context.BaseContext;
 import com.sky.dto.CategoryDTO;
 import com.sky.dto.CategoryPageQueryDTO;
 import com.sky.entity.Category;
@@ -40,15 +39,8 @@ public class CategoryServiceImpl implements CategoryService {
     @Override
     public void save(CategoryDTO categoryDTO) {
         Category category = new Category();
-
         BeanUtils.copyProperties(categoryDTO, category);
-
         category.setStatus(StatusConstant.DISABLE);
-        category.setCreateTime(LocalDateTime.now());
-        category.setUpdateTime(LocalDateTime.now());
-        category.setCreateUser(BaseContext.getCurrentId());
-        category.setUpdateUser(BaseContext.getCurrentId());
-
         categoryMapper.insert(category);
     }
 
@@ -90,10 +82,6 @@ public class CategoryServiceImpl implements CategoryService {
     public void update(CategoryDTO categoryDTO) {
         Category category = new Category();
         BeanUtils.copyProperties(categoryDTO, category);
-
-        category.setUpdateTime(LocalDateTime.now());
-        category.setUpdateUser(BaseContext.getCurrentId());
-
         categoryMapper.update(category);
     }
 
@@ -107,8 +95,6 @@ public class CategoryServiceImpl implements CategoryService {
         Category category = Category.builder()
                 .id(id)
                 .status(status)
-                .updateTime(LocalDateTime.now())
-                .updateUser(BaseContext.getCurrentId())
                 .build();
         categoryMapper.update(category);
     }
